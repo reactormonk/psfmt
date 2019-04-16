e87a9b682b4915243bee64274a08915008adcb5c
Simon Hafner <hafnersimon@gmail.com>
2019-04-16 13:24:32 +0200

    Added binary

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # psfmt
 PureScript formatter
+
+# Usage
+
+For formatting every `.purs` file in the current directory:
+
+```
+psfmt
+```
+
+For formatting every `.purs` file in specific directories:
+
+```
+psfmt src test
+```
+
+For formatting a specific file:
+
+```
+psfmt File.purs
+```
--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,37 @@ module Main where
 
 import Lib
 import System.Environment
-import Data.Text.IO as T
+import qualified Data.Text.IO as T
+import qualified Data.Text as T
+import Language.PureScript.CST.Errors
+import System.Directory
+import System.Directory.PathWalk
+import Data.List (isSuffixOf)
+import System.FilePath
+
 import Relude
 
 main :: IO ()
 main = do
   args <- getArgs
-  let filename = "filename"
+  let
+    dirsOrFiles = if length args > 0 then args else ["."]
+    findMoreFiles directory = do
+      pathWalkAccumulate directory $ \dir subdirs files ->
+        pure $ fmap (\f -> dir </> f) $ filter (isSuffixOf ".purs") files
+  files <- filterM doesFileExist dirsOrFiles
+  dirs <- filterM doesDirectoryExist dirsOrFiles
+  filesInDirs <- fmap join $ traverse findMoreFiles dirs
+  let toFormat = files <> filesInDirs
+  traverse_ formatFile toFormat
+  T.hPutStr stdout $ "Formatted " <> (show $ length toFormat) <> " files\n"
+
+formatFile :: FilePath -> IO ()
+formatFile filename = do
   content <- T.readFile filename
-  T.writeFile filename content
+  case format content of
+    Left e ->
+      T.hPutStr stderr $
+        "Errors while formatting file " <> (T.pack filename) <> ": " <> (T.pack $ show e)
+    Right output ->
+      T.writeFile filename output
