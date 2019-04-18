b308df0e819f8aaf33718b65bacdbd66c46336b6
Simon Hafner <hafnersimon@gmail.com>
2019-04-18 17:36:43 +0200

    Release scripts

--- mkrelease.sh
+++ mkrelease.sh
@@ -5,9 +5,12 @@ git describe --tags --exact-match || exit 1
 GIT_VERSION=$(git describe --tags --exact-match)
 VERSION=${GIT_VERSION:1}
 OS=linux
-
 DIR=psfmt-$VERSION-$OS
-BIN_DIR=$DIR/bin
-mkdir -p BIN_DIR
-stack install --local-bin-path $BIN_DIR
-tar cf $BIN_DIR
+BIN_DIR=release/$DIR/bin
+RELEASE_DIR=release/$DIR
+
+stack install --local-bin-path $BIN_DIR --ghc-options '-optl-static -fPIC -Os'
+cp README.md $RELEASE_DIR
+pushd release
+tar --create --file=$DIR.tar.gz $DIR
+popd
